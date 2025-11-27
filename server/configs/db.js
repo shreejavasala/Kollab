7fee1d26afad04b2245a6c7beca80cccc6d66ffe
shreejavasala <shreevasala29@gmail.com>
2025-11-27 15:15:28 +0530

    db.js update

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,19 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+
+let isConnected = false;
 
 const connectDB = async () => {
-  try {
-    mongoose.connection.on('connected', () => console.log(`MongoDB connected successfully`))
-    await mongoose.connect(`${process.env.MONGO_URI}/kollab`)
-  } catch (error) {
-    console.log(`Error connecting to Database: ${error.message}`)
-  }
-}
+  if (isConnected) return;
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI not set");
 
-export default connectDB
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/kollab`);
+    isConnected = true;
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+};
+
+export default connectDB;
